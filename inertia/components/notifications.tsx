42e6894b7ef72d63d63ba5539c3c13f3f10f63c8
Lahgolz <lahgolz@proton.me>
2025-06-21 19:24:21 +0200

    fix: notifications

--- inertia/components/notifications.tsx
+++ inertia/components/notifications.tsx
@@ -3,7 +3,7 @@ import { useEffect } from 'react';
 import { usePage } from '@inertiajs/react';
 import { toast } from 'sonner';
 
-import { NotificationType, type NotificationFlash } from '#types/notification';
+import type { NotificationFlash } from '#types/notification';
 
 import { useMounted } from '~/hooks/use-mounted';
 import { handleNotification } from '~/lib/handle-notification';
@@ -11,27 +11,18 @@ import { handleNotification } from '~/lib/handle-notification';
 import { Toaster } from './ui/sonner';
 
 export function Notifications() {
-	const { notification, errors } = usePage().props;
+	const { notification } = usePage().props;
 	const mounted = useMounted();
 
 	useEffect(() => {
-		if (!mounted || !(notification && (errors as unknown))) {
+		if (!mounted || !notification) {
 			return;
 		}
 
-		const errorMessages = Object.values(errors).filter((errorMessage) => typeof errorMessage === 'string') as string[];
-
-		const toastIds =
-			errorMessages.length > 0
-				? errorMessages.map((errorMessage) =>
-						handleNotification({ message: errorMessage, type: NotificationType.Error }),
-					)
-				: [handleNotification(notification as NotificationFlash)];
+		const id = handleNotification(notification as NotificationFlash);
 
 		return () => {
-			for (const toastId of toastIds) {
-				toast.dismiss(toastId);
-			}
+			toast.dismiss(id);
 		};
 	}, [notification, mounted]);
 
